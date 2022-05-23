c87606bdfa691b32b6b4e909670b7b0566554c97
Kalashov Luka <102518316+lkalashov@users.noreply.github.com>
2022-05-23 14:58:03 +0400

    Hide slideOutView on Android

--- Swipeable/SwipeableRow.js
+++ Swipeable/SwipeableRow.js
@@ -184,7 +184,7 @@ class SwipeableRow extends React.Component {
                 <View style={[
                     styles.slideOutContainer, 
                     { height: this.state.rowHeight }, 
-                    !this.props.isOpen ? { display: "none" } : null
+                    !this.props.isOpen ? { display: "none", opacity: 0 } : null
                 ]}>
                     {this.props.slideoutView}
                 </View>
