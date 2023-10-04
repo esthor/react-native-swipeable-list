b26f9732f6a995ae6ff6e4d645c6fe6b556595e5
Saul Lee <solleedata@gmail.com>
2023-10-04 11:47:43 +0900

    Add type definition (react-native-swipeable-list)

---
+++ types/react-native-swipeable-list/index.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="node" />
+/// <reference types="react"/>
+/// <reference types="react-native"/>
+
+declare module 'react-native-swipeable-list' {
+  import React from 'react';
+  import { FlatListProps } from 'react-native';
+
+  interface SwipeableFlatListProps<ItemT> extends FlatListProps<ItemT> {
+    maxSwipeDistance?: number;
+    shouldBounceOnMount?: boolean;
+    renderQuickActions: ({ index: number, item: ItemT }) => React.ReactNode;
+  }
+
+  class SwipeableFlatList<ItemT> extends React.Component<
+    SwipeableFlatListProps<ItemT>
+  > {}
+
+  export = SwipeableFlatList;
+}
